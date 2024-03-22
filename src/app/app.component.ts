81a959901ab2d8c73d98325e17526d937f0885b5
Lorenzo <lorenzo.porcu@etu.univ-grenoble-alpes.fr>
2024-03-22 11:13:01 +0100

    ajout plateau et modif appcomp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokerService } from './poker.service';
+import { carte, defaultCarte } from './type';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'poker_angular';
+
+  constructor(private pokerService : PokerService){};
+
+  private readonly sigJoueur1 = new BehaviorSubject<Set<carte>>(new Set());
+  private readonly sigJoueur2 = new BehaviorSubject<Set<carte>>(new Set());
+
+
+  async piocherJoueur(Idjoueur: number): Promise<void> {
+
+    const carteTiree = await this.pokerService.drawCards(2);
+    Idjoueur === 1 ? this.sigJoueur1.next(carteTiree) : this.sigJoueur2.next(carteTiree);
+  }
 }
---
+++ src/app/plateau/plateau.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plateau',
+  standalone: true,
+  imports: [],
+  templateUrl: './plateau.component.html',
+  styleUrl: './plateau.component.css'
+})
+export class PlateauComponent {
+
+}
